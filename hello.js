6a3508968e3e4d42547db841464380f433da238a
Necharul <necharulislam15@gmail.com>
2022-12-24 18:34:18 +0600

    ok

--- hello.js
+++ hello.js
@@ -1,5 +1,8 @@
 var bananaPrice = 12;
 console.log(bananaPrice);
+
+
+
 // Practice
 var butterBon = 8;
 var potatoChips = 10;
@@ -7,6 +10,9 @@ var tomato = 50;
 console.log(butterBon);
 console.log(potatoChips);
 console.log(tomato);
+
+
+
 // type
 console.log(typeof butterBon);
 var name = "kolimUddin Zuckerburg";
@@ -16,3 +22,21 @@ var isHot = true;
 var isRice = false;
 console.log(isHot);
 console.log(typeof isHot);
+
+
+
+// Practice
+var myAge = 26;
+var myBrithYear = 1998;
+console.log(myAge);
+console.log(typeof myAge);
+
+var myName = "Sagor";
+var myWork = "Web Developing";
+console.log(myWork);
+console.log(typeof myWork);
+
+var myLaptop = true;
+var myApplePhone = false;
+console.log(myApplePhone);
+console.log(typeof myApplePhone);
