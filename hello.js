2c1023e23e0d0029a85b90f084714b3edce3810c
Necharul <necharulislam15@gmail.com>
2022-12-24 18:26:19 +0600

    basic

---
+++ hello.js
@@ -0,0 +1,18 @@
+var bananaPrice = 12;
+console.log(bananaPrice);
+// Practice
+var butterBon = 8;
+var potatoChips = 10;
+var tomato = 50;
+console.log(butterBon);
+console.log(potatoChips);
+console.log(tomato);
+// type
+console.log(typeof butterBon);
+var name = "kolimUddin Zuckerburg";
+console.log(name);
+console.log(typeof name);
+var isHot = true;
+var isRice = false;
+console.log(isHot);
+console.log(typeof isHot);
