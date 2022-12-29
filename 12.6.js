e67e6089ce0d98dc8301389597e9d93abd01081b
Necharul <necharulislam15@gmail.com>
2022-12-29 10:45:41 +0600

    12.6

--- 12.6.js
+++ 12.6.js
@@ -7,3 +7,14 @@ console.log(number1 + number2);
 
 number2 = parseInt(number2);     //parseInt mean "Purno Songkha". ".5" k bad dia dibe
 console.log(number1 + number2);
+number1 = ""+number1; // for convert number to string
+console.log(typeof number1);
+var number1 = 0.1;
+var number2 = 0.2;
+console.log(number1 + number2);
+// ------------
+var number1 = 0.1;
+var number2 = 0.2;
+var total = number1 + number2;
+total = total.toFixed(5); // Doshomik por koto ghor porjonto dekhaby tai "toFixed" use korchi  
+console.log(total);
