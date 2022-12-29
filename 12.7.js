87427e206397bd5650049f33d1aa361f1668b76c
Necharul <necharulislam15@gmail.com>
2022-12-29 13:09:53 +0600

    12.7

---
+++ 12.7.js
@@ -0,0 +1,12 @@
+var price1 = 25;
+var price2 = 35;
+price2++;
+price2--; // decrease 1 of price2
+var total = price2 % price1;
+console.log(total);
+// -------------
+
+var price1 = 25;
+var name = 'alu';
+var result = name + ' ' + price1;
+console.log(result);
