542b393a41bfaf302932c0ef2edd3824f529ae15
Necharul <necharulislam15@gmail.com>
2022-12-31 17:16:11 +0600

    12.8

---
+++ 12.8.js
@@ -0,0 +1,43 @@
+var number = -5;
+var absoluteNumber = Math.abs(number);
+console.log(absoluteNumber);
+// -----------------
+
+
+
+
+
+// round
+var number = 5.5468;
+var result = Math.round(number);
+console.log(result);
+
+// --------------------
+
+
+
+
+// round korbe uporer dikhy
+var number = 5.468;
+var result = Math.ceil(number);
+console.log(result);
+// --------------------
+
+
+
+
+// round korbe nicher dikhy
+var number = 5.5468;
+var result = Math.floor(number);
+console.log(result);
+// ------------------
+
+
+// random
+var number = 5.5468;
+var result = Math.random(number);
+console.log(result);
+// or, randomvar number = 5.5468;
+var number = Math.random() * 100;
+var result = Math.round(number);
+console.log(result);
