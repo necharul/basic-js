d5c25507b33142ee963bef00f3b43aede070bdb9
Necharul <necharulislam15@gmail.com>
2023-01-01 15:01:37 +0600

    12.9

---
+++ 12.9.conditional.js
@@ -0,0 +1,11 @@
+var biscuitPrice = 8;
+if(biscuitPrice < 10){
+    console.log("I will eat biscuit");
+}
+else{
+    console.log("khali cha is better");
+};
+
+// if(biscuitPrice > 10)
+// if(biscuitPrice == 10)   Equal
+//  if(biscuitPrice != 10) Not Equal
